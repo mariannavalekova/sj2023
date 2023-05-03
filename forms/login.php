7f21b6fb655cbed679d0801c57b81ee3e423e262
mariannavalekova <131619121+mariannavalekova@users.noreply.github.com>
2023-05-03 17:23:49 +0200

    Add files via upload

---
+++ forms/login.php
@@ -0,0 +1,26 @@
+<div class="user_login">
+    <form action="" method="post">
+        <label>Email / Username</label>
+        <input name="username" type="text" id="username"/>
+        <br/>
+
+        <label>Password</label>
+        <input name="password" type="password" id="password"/>
+        <br/>
+
+        <div class="checkbox">
+            <input id="remember" type="checkbox"/>
+            <label for="remember">Remember me on this computer</label>
+        </div>
+
+        <div class="action_btns">
+            <div class="one_half"><a href="#" class="btn back_btn"><i class="fa fa-angle-double-left"></i> Back</a>
+            </div>
+            <div class="one_half last">
+                <button type="submit" class="btn btn_red">Login</button>
+            </div>
+        </div>
+    </form>
+
+    <a href="#" class="forgot_password">Forgot password?</a>
+</div>
